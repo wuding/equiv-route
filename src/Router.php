99680167a572348e4b53443f79c6b95a313f6075
wuding <netjoin@gmail.com>
2019-08-17 00:25:32 +0800

    修正缩进为空格

--- src/Router.php
+++ src/Router.php
@@ -4,58 +4,55 @@ namespace EquivRoute;
 
 class Router
 {
-	public $adpater = null;
+    public $adpater = null;
 
-	public function __construct($name = 'wuding/equiv-route', $routes = [], $options = [])
-	{
-		$names = [
-			'wuding/equiv-route' => 'EquivRoute',
-			'nikic/fast-route' => 'FastRoute',
-		];
+    public function __construct($name = 'wuding/equiv-route', $routes = [], $options = [])
+    {
+        $names = [
+            'wuding/equiv-route' => 'EquivRoute',
+            'nikic/fast-route' => 'FastRoute',
+        ];
 
-		if (array_key_exists($name, $names)) {
-			$file = $names[$name];
-			$class = "EquivRoute\\Adpater\\$file";
-			$this->adpater = new $class($options, $routes);
+        if (array_key_exists($name, $names)) {
+            $file = $names[$name];
+            $class = "EquivRoute\\Adpater\\$file";
+            $this->adpater = new $class($options, $routes);
 
-			if ($routes) {
-				# $this->addRoutes($routes);
-			}
-		}
-	}
+            if ($routes) {
+                # $this->addRoutes($routes);
+            }
+        }
+    }
 
-	public function addRoutes($routes)
-	{
-		if (is_array($routes)) {
-			foreach ($routes as $route) {
-				$this->addRoute($route[0], $route[1], $route[2]);
-			}
-		}
-		
-	}
+    public function addRoutes($routes)
+    {
+        if (is_array($routes)) {
+            foreach ($routes as $route) {
+                $this->addRoute($route[0], $route[1], $route[2]);
+            }
+        }
+    }
 
-	public function addRoute($httpMethod, $route, $handler)
-	{
-		$this->adpater->addRoute($httpMethod, $route, $handler);
-	}
+    public function addRoute($httpMethod, $route, $handler)
+    {
+        $this->adpater->addRoute($httpMethod, $route, $handler);
+    }
 
-	public function getData()
-	{
-		return $this->adpater->getData();
-	}
-	
-	public function addGroup()
-	{
-		
-	}
-		
-	public function parse()
-	{
-		
-	}
-	
-	public function dispatch($httpMethod, $uri)
-	{
-		return $this->adpater->dispatch($httpMethod, $uri);
-	}
+    public function getData()
+    {
+        return $this->adpater->getData();
+    }
+
+    public function addGroup()
+    {
+    }
+
+    public function parse()
+    {
+    }
+
+    public function dispatch($httpMethod, $uri)
+    {
+        return $this->adpater->dispatch($httpMethod, $uri);
+    }
 }
