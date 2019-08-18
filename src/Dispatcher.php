609605ffad07f3ec2c4cf6810d5d2464045c227d
wuding <netjoin@gmail.com>
2019-08-18 19:39:28 +0800

    添加虚拟路径索引文件支持

--- src/Dispatcher.php
+++ src/Dispatcher.php
@@ -13,6 +13,7 @@ class Dispatcher
     {
         # print_r(get_defined_vars());
 
+        $uri = $this->virtualPath($uri);
         # $status = 0;
         $result = array($status);
         if (1 === $status) {
@@ -22,6 +23,32 @@ class Dispatcher
         } elseif (2 === $status) {
             $result[1] = array();
         }
+        return $result;
+    }
+
+    public function virtualPath($uri)
+    {
+        global $_CONFIG;
+        $PATH = $_CONFIG['virtual_paths'];
+        $str = ltrim($uri, '/');
+        $len = mb_strlen($str);
+
+        $result = $uri;
+        $index = null;
+        $paths = isset($PATH[$len]) ? $PATH[$len] : null;
+        if ($paths) {
+            foreach ($paths as $key => $value) {
+                if (preg_match($key, $str)) {
+                    $index = $value;
+                    break;
+                }
+            }
+        }
+
+        if ($index) {
+            $result = include $index;
+        }
+        # print_r(get_defined_vars());
         return $result;
     }
 }
